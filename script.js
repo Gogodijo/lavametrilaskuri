150f6d07c8d2a1035717906c10fc478fa6620cc3
Joni Vilokki <36036801+Gogodijo@users.noreply.github.com>
2021-01-10 20:44:40 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let eur=0
+let fin=0
+let rll=0
+let dol=0
+let teho=0
+let vartti=0
+let mrlk=0
+let pienipullo=0
+let isopullo=0
+let kukkarullakko=0
+
+function calculateLvm(){
+    getValues()
+    console.log("calculatea kutsuttu")
+    console.log(eur)
+}
+
+function getValues(){
+
+}
+
+function reset(){
+
+}
