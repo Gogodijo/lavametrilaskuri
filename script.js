4df621f94dc63833def3bab8b7d5b1d9058d6bd2
Joni Vilokki <36036801+Gogodijo@users.noreply.github.com>
2021-01-10 20:59:44 +0200

    Buttonit lisätty

--- script.js
+++ script.js
@@ -5,9 +5,12 @@ let dol=0
 let teho=0
 let vartti=0
 let mrlk=0
+let mdol=0
 let pienipullo=0
 let isopullo=0
-let kukkarullakko=0
+let ccrlk=0
+
+
 
 function calculateLvm(){
     getValues()
@@ -16,9 +19,16 @@ function calculateLvm(){
 }
 
 function getValues(){
-
+    console.log("Get values")
 }
 
 function reset(){
+    console.log("Reset")
+    eur=0
+    fin=0
+    rll=0
+    dol=0
+    teho=0
+    vartti=0
 
 }
